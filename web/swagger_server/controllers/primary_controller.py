4ee54c633782504858471b00193756a5c00d1c82
bowei437 <bowei@vt.edu>
2017-04-16 17:08:09 -0400

    updated2

--- web/swagger_server/controllers/primary_controller.py
+++ web/swagger_server/controllers/primary_controller.py
@@ -53,8 +53,8 @@ def post_primary(version, problem):
     return 'do some magic!'
     """
     try:
-        str_body = str(problem.decode("utf-8")).replace('\'', '\"')
-        json.loads(str_body)
+        #str_body = str(problem.decode("utf-8")).replace('\'', '\"')
+        json.loads(problem)
         db_size = db.posts.count()+1
         print(str_body)
         problem = Body.from_dict(connexion.request.get_json())
